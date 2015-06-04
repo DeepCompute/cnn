73c431aa8ae43f30514f1d988649d15e6bb71f18
王刚 <wanggang@zxisl.com>
2015-06-04 19:56:59 +0800

    添加语音数据

--- src/main/java/info/hb/ccn/main/CNNSpeech.java
+++ src/main/java/info/hb/ccn/main/CNNSpeech.java
@@ -7,9 +7,15 @@ import info.hb.cnn.core.Layer.Size;
 import info.hb.cnn.data.DataSet;
 import info.hb.cnn.utils.ConcurentRunner;
 
-public class CNNMain {
+public class CNNSpeech {
 
-	private static final String MODEL_NAME = "model/model.cnn";
+	private static final String MODEL_NAME = "speech/model/model.cnn";
+
+	private static final String TRAIN_DATA = "speech/train.format";
+
+	private static final String TEST_DATA = "speech/test.format";
+
+	private static final String TEST_PREDICT = "speech/test.predict";
 
 	public static void main(String[] args) {
 
@@ -24,16 +30,15 @@ public class CNNMain {
 	public static void runTrain() {
 		// 构建网络层次结构
 		LayerBuilder builder = new LayerBuilder();
-		builder.addLayer(Layer.buildInputLayer(new Size(28, 28)));
-		builder.addLayer(Layer.buildConvLayer(6, new Size(5, 5)));
+		builder.addLayer(Layer.buildInputLayer(new Size(6, 6)));
+		builder.addLayer(Layer.buildConvLayer(2, new Size(3, 3)));
 		builder.addLayer(Layer.buildSampLayer(new Size(2, 2)));
-		builder.addLayer(Layer.buildConvLayer(12, new Size(5, 5)));
-		builder.addLayer(Layer.buildSampLayer(new Size(2, 2)));
-		builder.addLayer(Layer.buildOutputLayer(10));
+		//		builder.addLayer(Layer.buildConvLayer(3, new Size(3, 3)));
+		//		builder.addLayer(Layer.buildSampLayer(new Size(2, 2)));
+		builder.addLayer(Layer.buildOutputLayer(2));
 		CNN cnn = new CNN(builder, 10);
 		// 加载训练数据
-		String trainSet = "dataset/train.format";
-		DataSet dataset = DataSet.load(trainSet, ",", 784);
+		DataSet dataset = DataSet.load(TRAIN_DATA, ",", 36);
 		// 开始训练模型
 		cnn.train(dataset, 3);
 		// 保存训练好的模型
@@ -45,9 +50,9 @@ public class CNNMain {
 		// 加载训练好的模型
 		CNN cnn = CNN.loadModel(MODEL_NAME);
 		// 加载测试数据
-		DataSet testSet = DataSet.load("dataset/test.format", ",", -1);
+		DataSet testSet = DataSet.load(TEST_DATA, ",", -1);
 		// 预测结果
-		cnn.predict(testSet, "dataset/test.predict");
+		cnn.predict(testSet, TEST_PREDICT);
 		testSet.clear();
 	}
 
